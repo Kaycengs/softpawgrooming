af646c625cc62bac35c6ba9f5bc76af50281f495
Kaycengs <gabaldonkaycen@gmail.com>
2024-02-12 18:33:30 -0700

    yellow

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-yellow-300 bg-red-500 2x1 font-semibold`}>
             kaycen is learning{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
