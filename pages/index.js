489daa9a93f241d981b4f60642647ac9ce8eabc0
Kaycengs <gabaldonkaycen@gmail.com>
2024-02-16 16:20:33 -0700

    ..

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,17 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div
-      className=" uppercase font-semibold min-h-screen min-w-screen flex flex-col align-middle justify-center text-blue-800 bg-pink-500 bg-  text-2xl text-center
-"
-    >
-
+      className=" fit uppercase font-semibold min-h-screen min-w-screen flex flex-col align-middle justify-center text-blue-800 bg-pink-500 bg-  text-2xl text-center"
+      >
       <Image
-      alt=""
-      className=" full bottom-0 rounded-fill fixed hover:hue-rotate-60 hover:rotate-45 duration-1000 hover:scale-150 z-0 "
-      src="/"
+      alt="Logo"
+      src="/dog bath.gif"
+      className="align center"
       height={1}
-      width={1080}
+      width={1920}
     />
+    
       <div></div>
-      
     </div>
   );
 }
